0052deb6fc0d9d50391dbbd6bb536729a9a7c166
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-10 11:57:40 +0530

    Type Bounded fixed here

--- GenericTypeBounded.java
+++ GenericTypeBounded.java
@@ -1,7 +1,35 @@
-public class GenericTypeBounded implements InnerGenericTypeBounded <MainType>{
+public class GenericTypeBounded implements InnerGenericTypeBounded<MainType> {
+
+    private MainType mainType;
+
+    // Constructor
+    public GenericTypeBounded(MainType mainType) {
+        this.mainType = mainType;
+    }
+
     public static void main(String[] args) {
         MainType mainType = new MainType(1, 58000, "Pallavi Purushottam Gutthe");
- 
+        GenericTypeBounded generic = new GenericTypeBounded(mainType);
+
+        // Display the details using the show() method
+        generic.show(mainType);
+
+        // Retrieve and display data using the get() method
+        MainType retrievedMainType = generic.get();
+        System.out.println("Retrieved: " + retrievedMainType);
+    }
+
+    @Override
+    public void show(MainType type) {
+        System.out.println("Details of MainType: ");
+        System.out.println("ID: " + type.getNum());
+        System.out.println("Salary: " + type.getSal());
+        System.out.println("Name: " + type.getName());
+    }
+
+    @Override
+    public MainType get() {
+        return new MainType(12, 40000, "Puru");
     }
 }
 
@@ -10,20 +38,32 @@ interface InnerGenericTypeBounded<Type extends MainType> {
     Type get();
 }
 
-class MainType{
+class MainType {
     private int num;
     private int sal;
     private String name;
-    MainType(int num, int sal, String name){
-        this.name=name;
-        this.sal=sal;
-        this.num=num;
+
+    public MainType(int num, int sal, String name) {
+        this.num = num;
+        this.sal = sal;
+        this.name = name;
     }
 
-    //MainType mainType = new MainType(12, 40000, "Puru");
-
-    public MainType getData(){
-        return new MainType(12, 40000, "Puru");
+    // Getters
+    public int getNum() {
+        return num;
     }
 
+    public int getSal() {
+        return sal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "MainType [num=" + num + ", sal=" + sal + ", name=" + name + "]";
+    }
 }
